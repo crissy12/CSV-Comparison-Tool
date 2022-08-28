446869e1aec4d50d4534c802a3540fcf97aae630
Cristian <cristiangcs45@gmail.com>
2022-08-28 10:57:16 -0400

    Added functionality to compare csv sheets

--- csvtool.py
+++ csvtool.py
@@ -1,20 +1,26 @@
+from email import message
 from struct import pack
 from tkinter import *
 from tkinter import filedialog as fd
 import tkinter
 from tkinter.ttk import *
+import csv
+import re
 
 csvfiles = []
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 root = Tk()
 root.geometry('200x100')
 label = Label(root, text="CSV Selector")
 root.title('CSV Comparison Tool')
-
+fusemail_emails = []
+active_emails = []
 
 def file_selection():
     filetypes = (
-        ('text files', '*.txt'),
+        ('csv files', '*.csv'),
         ('All files', '*.*')
     )
 
@@ -22,17 +28,42 @@ def file_selection():
         title='Open a file',
         initialdir='/',
         filetypes=filetypes)
+    return filename
+
+def fusemail_action():
+    filename = file_selection()
+    get_info (fusemail_emails,filename)
+   
+
+def active_action():
+    filename = file_selection()
+    get_info (active_emails,filename)
     
-    csvfiles.append(filename)
 
-button1 = Button(root, text = 'Select your first CSV', command = file_selection)
-button2 = Button(root, text = 'Select your second CSV', command = file_selection)
-button3 = Button(root, text = 'Process Files / Close Window', command = root.destroy)
 
-button1.pack(side=tkinter.TOP)
-button2.pack(side=tkinter.TOP)
-button3.pack(side=tkinter.BOTTOM)  
+def get_info(list_to_use,filename):
+    with open (str(filename)) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            list_to_use.extend(list(filter(lambda x: re.match(regex,x),row)))
+    return (list_to_use)
+
+def process_files():
+    print('\n\n******************************************************************************\n')
+    print('******************************************************************************\nItems to remove from Fusemail\n')
+    for fusemail_item in fusemail_emails:
+        if fusemail_item not in active_emails:
+            print (fusemail_item)
+    
+
+first_csv_button = Button(root, text = 'Select CSV from Fusemail', command = fusemail_action)
+second_csv_button = Button(root, text = 'Select CSV from 365', command = active_action)
+submit_button = Button(root, text = 'Submit/Process', command = process_files)
+close_button = Button(root, text = 'Close Window', command = root.destroy)
+
+first_csv_button.pack(side=tkinter.TOP)
+second_csv_button.pack(side=tkinter.TOP)
+submit_button.pack(side=tkinter.TOP)
+close_button.pack(side=tkinter.BOTTOM)  
 
 root.mainloop()
-
-print(csvfiles)
