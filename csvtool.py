de2660a67a72c48271d39521bc5136e28f196cb4
Cristian <cristiangcs45@gmail.com>
2022-09-04 08:43:55 -0400

    Added safe guards and printing out to txt file

--- csvtool.py
+++ csvtool.py
@@ -6,13 +6,12 @@ import tkinter
 from tkinter.ttk import *
 import csv
 import re
+from types import NoneType
+import os
 
 '''
 Issues to investigate:
-1. Add logic for when user cancels the file search.
-2. Investigate issues with program freezing sometimes.
-3. Add logic to print results to a text file and then open it automatically for the user.
-4. Add logic to not accept the SMTP email addresses that may show up in the 365 reports.
+1. Add logic to not accept the SMTP email addresses that may show up in the 365 reports.
 '''
 
 # Reg Expression to filter out email address on the CSV sheet.
@@ -31,8 +30,7 @@ def file_selection():
     filetypes = (
         ('csv files', '*.csv'),
         ('All files', '*.*')
-    )
-
+    )   
     filename = fd.askopenfilename(
         title='Open a file',
         initialdir='/',
@@ -42,11 +40,15 @@ def file_selection():
 #function to get filename/path from the file selection function and then pass that to the get info function with filename info and the corresponding array.
 def fusemail_action():
     filename = file_selection()
+    if not filename: # Checks for empty string
+        return 
     get_info (fusemail_emails,filename)
    
 #same function as above but for the other csv files.
 def active_action():
     filename = file_selection()
+    if not filename: # Checks for empty string
+        return 
     get_info (active_emails,filename)
     
 # Gets the array to use and filename/path to use and then opens the CSV file and gets the information from the csv.
@@ -59,11 +61,14 @@ def get_info(list_to_use,filename):
     return (list_to_use)
 #Process the arrays and checks if there are emails in the fusemail array that are not in the active emails array.
 def process_files():
-    print('\n\n******************************************************************************\n')
-    print('******************************************************************************\nItems to remove from Fusemail\n')
-    for fusemail_item in fusemail_emails:
-        if fusemail_item not in active_emails:
-            print (fusemail_item)
+    with open('results.txt','w') as output:
+        output.write('******************************************************************************\n')
+        output.write('Items to remove from Fusemail\n******************************************************************************\n')
+        for fusemail_item in fusemail_emails:
+            if fusemail_item not in active_emails:
+                output.write (fusemail_item)
+                output.write('\n')
+    os.startfile('results.txt')
     
 # Button declarations
 first_csv_button = Button(root, text = 'Select CSV from Fusemail', command = fusemail_action)
